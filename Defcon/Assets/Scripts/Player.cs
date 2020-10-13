78546d8f6909b7110f2d844e28f830ee5dc7603d
Pamela Petterchak <pamela.petterchak@oasisdigital.com>
2020-10-13 09:06:11 -0500

    added framework for player and simple AI

---
+++ Defcon/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player 
+{
+  public void start_turn()
+  {
+    //complete actions to signal beginning of turn
+  }
+
+  public void yield_turn()
+  {
+    //complete actions to signal end of turn
+  }
+
+}
