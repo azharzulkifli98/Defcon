703d259f7ad93c098ae28bcad7cbb4cb98d49393
Ethan Galloway <ecghdh@umsystem.edu>
2020-10-14 10:12:25 -0500

    World Renderer Half Point
    Pseudocode for much of the World Rendering module

---
+++ Defcon/Assets/Scripts/WorldChunk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldChunk : MonoBehaviour
+{
+    MeshCollider collider;
+
+    MeshRenderer renderer;
+
+    //TODO: Add additional "Board" argument
+    /// <summary>
+    /// Renders the world from [minX, maxX) and [minY, maxY)
+    /// </summary>
+    /// <param name="minX"> The lowest X value that is rendered</param>
+    /// <param name="maxX"> The lowest X value that isn't rendered after minX</param>
+    /// <param name="minY"> The lowest Y value that is rendered</param>
+    /// <param name="maxY"> The lowest Y value that isn't rendered after minY</param>
+    public void Prime(int minX, int maxX, int minY, int maxY)
+    {
+        for(int i = minX; i < maxX; i++)
+        {
+            for(int j = minY; j < maxY; j++)
+            {
+                RenderTile(i, j);
+            }
+        }
+    }
+
+    public void RenderTile(int x, int y)
+    {
+
+    }
+}
---
+++ Defcon/Assets/Scripts/WorldRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRenderer
+{
+    const int MAX_DIM = 10;
+    
+
+    public static void Render()
+    {
+        // for: Every MAX_DIM x MAX_DIM piece
+            // Create a chunk
+            // Prime the chunk
+    }
+}
