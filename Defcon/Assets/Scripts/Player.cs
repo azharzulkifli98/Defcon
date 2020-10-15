47f00c898ba1c5c213ff448d53974a7bbddd3a44
Pamela Petterchak <pamela.petterchak@oasisdigital.com>
2020-10-14 20:51:55 -0500

    pseudocode for simpleAI

--- Defcon/Assets/Scripts/Player.cs
+++ Defcon/Assets/Scripts/Player.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public abstract class Player //abstract denotes that this class has abstract functions and cannot be instantiated as a complete object+
 {
+    Board playerBoard = null;
+
+    Board enemyBoard = null;
+    
     /// <summary>
     ///complete actions to signal beginning of turn
     /// </summary>
-    public abstract void start_turn();  //abstract denotes that the function need not be implemented
+    public abstract void make_decision();  //abstract denotes that the function need not be implemented
 
     /// <summary>
     /// complete actions to signal end of turn
     /// </summary>
-    public abstract void yield_turn();
+    public abstract void end_decision();
 
 }
--- Defcon/Assets/Scripts/SimpleAI.cs
+++ Defcon/Assets/Scripts/SimpleAI.cs
@@ -1,12 +1,17 @@
 public class SimpleAI : Player
 {
-    public override void start_turn()   ///The override denotes that this function is an implementation of an abstract function
+    public override void make_decision()   ///The override denotes that this function is an implementation of an abstract function
     {
-        //actions for simple AI to start turn
+        //check all tiles on personal board for available silos 
+        //check all tiles on enemy board for highest population
+        //make sure it doesn't fire all three missiles at the same place
+        //FIRE Missiles through the missile manager
+        //if no missiles left, end_decision
+        //if missiles are fired, end_decision
         throw new System.NotImplementedException();
     }
 
-    public override void yield_turn()
+    public override void end_decision()
     {
         //actions for simple AI to start turn
         throw new System.NotImplementedException();
