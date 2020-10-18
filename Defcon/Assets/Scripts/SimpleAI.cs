ed5450528326c9bc30935bd1d716fb6732648c80
Pamela Petterchak <pamela.petterchak@oasisdigital.com>
2020-10-18 09:16:20 -0500

    consolidated decision into one loop
    still needs progress on making sure the same place isn't ht three times in one turn (though the dumb AI may be the type of thing to do that.)

--- Defcon/Assets/Scripts/SimpleAI.cs
+++ Defcon/Assets/Scripts/SimpleAI.cs
@@ -3,61 +3,29 @@ using System.Collections.Generic;
 
 public class SimpleAI : Player
 {
-    public override void make_decision(Board playerBoard, Board enemyBoard)   ///The override denotes that this function is an implementation of an abstract function
+    public override void make_decision(Board playerBoard, Board enemyBoard)   
     {
         //check all tiles on personal board for available silos
         List<MissileSilo> allSilos = playerBoard.GetAllSilos();
-        // ***Q: Can I assume all missles are NOT destroyed?***
 
-        if (allSilos.Count == 0){
-            // No missiles, end decision with no firing to do!
-            this.end_decision();
-            return;
-        }
-
-        // Does target default to 0,0 ? -Azhar
         BoardTile target = enemyBoard.GetTile(0, 0);
 
-        BoardTile target_one = enemyBoard.GetTile(0,0);
-        //check all tiles on enemy board for highest population
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                if (target_one.GetPopulation() < enemyBoard.GetTile(i,j).GetPopulation())
-                    target_one = enemyBoard.GetTile(i,j);
+        // shoot a missile from each available silo, based on highest population
+        // Issue --- need to make sure that the same place isn't shot at multiple times -Pamela
+        for(int k = 0; k < allSilos.Count; k++){
+            //check to make sure silo isn't empty or destroyed
+            if(allSilos[k].Fire_Missiles())
+            {
+                for(int i = 0; i < 10; i++){
+                    for(int j = 0; j < 10; j++){
+                        if (target.GetPopulation() < enemyBoard.GetTile(i,j).GetPopulation())
+                            target = enemyBoard.GetTile(i,j);
+                    }
+                }
+                enemyBoard.getMissileManager().RegisterMissile(target.x, target.y);
             }
         }
-        
-        BoardTile target_two = enemyBoard.GetTile(0,0);
-        if(target_one == target_two){
-            target_two = enemyBoard.GetTile(0,1);
-        }
-        //check all tiles on enemy board for second-highest population
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                if (target.GetPopulation() < enemyBoard.GetTile(i,j).GetPopulation())
-                    if (target_one != enemyBoard.GetTile(i,j))
-                        target = enemyBoard.GetTile(i,j);
-            }
-        }
-
-        BoardTile target_three = enemyBoard.GetTile(0,0);
-        //***this logic needs cleaned up***
-        if(target_one == target_three || target_two == target_three){
-            target_three = enemyBoard.GetTile(0,2);
-        }
-        //check all tiles on enemy board for third highest population
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                if (target.GetPopulation() < enemyBoard.GetTile(i,j).GetPopulation())
-                    if(target_one != enemyBoard.GetTile(i,j))
-                        target = enemyBoard.GetTile(i,j);
-            }
-        }
-    
-        //FIRE Missiles through the missile manager
-        //Not sure how to do this?
-
-        this.end_decision();
+        end_decision();
         return;
     }
 
