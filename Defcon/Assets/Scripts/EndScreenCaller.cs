6e98e2cc08abbd2467937c6ec2443e84b5f5eaf6
Ethan Galloway <ecghdh@umsystem.edu>
2020-11-01 17:06:23 -0600

    EndScreen Basic

---
+++ Defcon/Assets/Scripts/EndScreenCaller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class EndScreenCaller
+{
+    static string message;
+
+    public static string GetMessage()
+    {
+        return message;
+    }
+
+    public static void CallEndScreen(string message)
+    {
+        EndScreenCaller.message = message;
+
+        //TODO: Load Scene with EndScreenRenderer
+    }
+}
