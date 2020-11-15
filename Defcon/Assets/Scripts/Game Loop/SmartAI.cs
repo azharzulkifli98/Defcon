bacea5fa738d7d1fd2f63a5830edc7dcf7c7cd9b
Ethan Galloway <ecghdh@umsystem.edu>
2020-11-15 16:54:08 -0600

    Update SmartAI.cs

--- Defcon/Assets/Scripts/Game Loop/SmartAI.cs
+++ Defcon/Assets/Scripts/Game Loop/SmartAI.cs
@@ -7,6 +7,9 @@ public class SmartAI : Player
     // General idea is to target spot with highest point gain
     // right now it just what the simple AI did
 
+    // keeps a list of which tiles have been hit
+    HashSet<BoardTile> hitTiles = new HashSet<BoardTile>();
+
     // make decide whether it wants to :
     // fire a missile at a population
     // fire a missile at a structure
@@ -17,9 +20,6 @@ public class SmartAI : Player
         // holds all silos available to player
         List<MissileSilo> allSilos = playerBoard.GetAllSilos();
 
-        // keeps a list of which tiles have been hit
-        HashSet<BoardTile> hitTiles = new HashSet<BoardTile>(); 
-
         // stores enemy target tile, defaulted to 0,0
         BoardTile target = enemyBoard.GetTile(0, 0);
 
@@ -27,8 +27,8 @@ public class SmartAI : Player
         for(int k = 0; k < allSilos.Count; k++){
             if(allSilos[k].Can_Fire_Missile())
             {
-                for(int i = 0; i < 10; i++){
-                    for(int j = 0; j < 10; j++){
+                for(int i = 0; i < playerBoard.GetWidth(); i++){
+                    for(int j = 0; j < playerBoard.GetHeight(); j++){
                         if (target.GetDiscovered())
                         {
                             if ((target.GetPopulation() < enemyBoard.GetTile(i, j).GetPopulation()) && !hitTiles.Contains(target))
